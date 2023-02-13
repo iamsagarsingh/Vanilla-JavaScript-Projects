12e39a93391e27a9009d006bda4dd9982fdefd86
sagar <singh.sagar@protonmail.com>
2023-02-14 03:09:08 +0530

    added localstorgae feature to seats

--- movie-booking-app/script.js
+++ movie-booking-app/script.js
@@ -1,16 +1,31 @@
 const container = document.querySelector('.container');
 const totalSeats = document.getElementsByClassName('total-seats')[0]
 const totalCost = document.getElementsByClassName('total-cost')[0]
+const allSeats = document.querySelectorAll('.row .seat:not(.occupied)')
 const select = document.querySelector('#select')
 
 let total = +select.value;
 
+function populateUI(){
+    let mySeats = JSON.parse(localStorage.getItem('seats'))
+    mySeats.forEach(function(seat){
+        [...allSeats][seat].classList.toggle('selected')
+    })
+    
+}
 
+populateUI()
 
 function updateValues(){
     const selectedSeats = document.querySelectorAll('.row .selected');
     totalSeats.innerText = selectedSeats.length;
     totalCost.innerText = total*selectedSeats.length;
+    seatsArray = [...selectedSeats].map(seat=>{
+        return [...allSeats].indexOf(seat)
+    })
+    
+    localStorage.setItem('seats',JSON.stringify(seatsArray))
+
 }
 
 
